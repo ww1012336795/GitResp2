1d1eb7cf26f7917aea02c525abd640866104725d
Shadow <Shadow@163.com>
2024-02-29 22:06:48 +0800

    在Person类中增加了age属性

---
+++ TestMoudle01/src/com/shadow/Person.java
@@ -0,0 +1,11 @@
+package com.shadow;
+
+/**
+ * @Auther: shadow
+ * @Date: 2024/2/29 - 02 - 29 - 22:06
+ * @Description: com.shadow
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
